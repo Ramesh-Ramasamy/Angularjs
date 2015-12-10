7cd50f79396bec82246fef99f04f977417ff6383
ramesh <ramesh.r@infibeam.net>
2015-12-10 11:17:53 +0530

    javascript can be spllited

---
+++ mainjs.js
@@ -0,0 +1,8 @@
+function disFrame(){		
+	document.getElementById("imgSliding").style.display="none";
+	document.getElementById("infiFrame").style.display="block";
+}
+function imgSliding(){
+	document.getElementById("infiFrame").style.display="none";
+	document.getElementById("imgSliding").style.display="block";
+}
