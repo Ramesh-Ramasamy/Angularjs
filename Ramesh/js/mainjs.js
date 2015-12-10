7a9ddacd0dc5dc134182d6ec43b7564e298ead99
Ramesh-Ramasamy <ramesh.r@infibeam.net>
2015-12-10 16:45:20 +0530

    changed onclick to addEventListener

--- Ramesh/js/mainjs.js
+++ Ramesh/js/mainjs.js
@@ -1,8 +1,10 @@
-function disFrame(){		
+var iFrame = document.getElementById("disFrame");
+var imgSlide = document.getElementById("imgSlide");
+iFrame.addEventListener("click",function(){    	
 	document.getElementById("imgSliding").className="dontDisplay";
 	document.getElementById("infiFrame").className="disblock";
-}
-function imgSliding(){
+});
+imgSlide.addEventListener("click",function(){    	
 	document.getElementById("infiFrame").className="dontDisplay";
 	document.getElementById("imgSliding").className="disblock";
-}
+});
