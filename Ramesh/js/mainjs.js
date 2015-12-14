94145f8ff18eeccf1886656961e3bd31c3e7de5d
Ramesh-Ramasamy <ramesh.r@infibeam.net>
2015-12-14 17:20:56 +0530

    bugfix:not set id in initial section

--- Ramesh/js/mainjs.js
+++ Ramesh/js/mainjs.js
@@ -11,20 +11,26 @@ function addAEL(idFName,idIName){
 	});
 }
 function multipleSection (secTag) {
-	var i=-1;
+	var i = 0;
+	addAEL (document.getElementById(document.querySelectorAll('button')[i].id),
+				document.getElementById(document.querySelectorAll('button')[i+1].id));
 	return function(count){		
-		for(var j=0;j<count;j++){			
-			var buttons = document.querySelectorAll('button');
-			buttons[++i].id = i;
-			var idFName = document.getElementById(buttons[i].id);
-			buttons[++i].id = i;
-			var idIName = document.getElementById(buttons[i].id);									
-			addAEL (idFName,idIName);
+		for(var j=0;j<count;j++){
 			var section = document.createElement("section");
 			section.innerHTML = secTag[0].innerHTML;
-			document.body.appendChild(section);						
+			document.body.appendChild(section);					
+			var buttons = document.querySelectorAll('button');
+			buttons[(++i)+1].id = i;
+			var idFName = document.getElementById(buttons[i+1].id);
+			buttons[(++i)+1].id = i;
+			var idIName = document.getElementById(buttons[i+1].id);									
+			addAEL (idFName,idIName);						
 		}				
 	}
 }
 var mSection = multipleSection(document.getElementsByTagName("section"));
 mSection(5);
+
+
+/*var messages = ["hi how are you", "a","b"]
+create_nav(messages)*/
