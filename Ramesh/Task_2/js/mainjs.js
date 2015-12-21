548bfa211ef3793dd195c8ee7ac58e4f04d7508f
Ramesh-Ramasamy <ramesh.r@infibeam.net>
2015-12-21 12:49:44 +0530

    Task 3 completed in jquery

--- Ramesh/Task_2/js/mainjs.js
+++ Ramesh/Task_2/js/mainjs.js
@@ -1,18 +1,11 @@
-(function(){
+$(function(){
 	var country = ["India","America","Australia","china","Japan"];
-	for(var i=0;i<country.length;i++){
-		var parent = document.getElementById("buttons");
-		var button = document.createElement("button");
-		button.innerHTML = country[i];
-		parent.appendChild(button);
-		button.addEventListener("click",show_me(i));
-		var br_tag = document.createElement("br")
-		parent.appendChild(br_tag);
-	}
-	function show_me(i){
-		var count=0;
-		return function(){
-			document.getElementById("show").innerHTML = country[i]+"-"+(++count);
-		}
-	}
-})();
+	var total_count = 0;	
+	$.each(country,function(index,item){		
+		$("#buttons").append($(document.createElement("button")).text(item).attr('id',index));										
+		var count = 0;		
+		$("button[id="+index+"]").click(function(){			
+			$("#show").text(item+"-"+(++count)+"-Total Count-"+(++total_count)).fadeOut(100).fadeIn(100);	
+		});		
+	});	
+});
