0d622591088b2f7c5ffea02251ad60bca975dd4a
Ramesh-Ramasamy <ramesh.r@infibeam.net>
2016-01-04 11:18:37 +0530

    checking to not in id

--- Ramesh/Task_3/js/mainjs.js
+++ Ramesh/Task_3/js/mainjs.js
@@ -4,7 +4,7 @@ $(function(){
 	$.each(country,function(index,item){		
 		$("#buttons").append($(document.createElement("button")).text(item).attr('id',index));										
 		var count = 0;		
-		$("button[id="+index+"]").click(function(){
+		$("button").click(function(){
 			$("#title").text(item.toUpperCase());
 			$("#showdetails").load("txtfile/"+item+".txt");		
 			$("#show").text(item+"-"+(++count)+"-Total Count-"+(++total_count)).fadeOut(100).fadeIn(100);	
