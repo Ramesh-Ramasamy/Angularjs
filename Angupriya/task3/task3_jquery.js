202ef111c48518cddba7388e68ba60cbd6ace431
Ramesh-Ramasamy <ramesh.r@infibeam.net>
2016-01-08 12:12:10 +0530

    rakefile modified to my application description

--- Angupriya/task3/task3_jquery.js
+++ Angupriya/task3/task3_jquery.js
@@ -1,20 +1,31 @@
 $(function()
 {
-	
-	var total_count=0;
-	var content=["abc def","cde","hgsd","jhsd","shd"];
-	$.each(content,function(index,value)
-	{   
-		var count=0;
-		$("#leftside").append($(document.createElement("button")).html(value).attr("id",index));
-		$("#"+index).click(function()
-		{
-
-			$("#contents").empty();
-
-			$("#contents").append("count value is : "+(++count)+"  total_count value : "+(++total_count)+"test value: "+test);
-
-		});
-
+	var content=["ABOUT_US :Infibeam Inc is a leading online retailer in India ","Infibeam_offers: cloud based  ecommerce platform service  business values and responsive to changing customer ne"];
+	$.each(content,function(index,value){
+		$("#leftside").append($(document.createElement("button")).html(value.slice(0,value.indexOf(" "))));
 	});
+	var count = 0;
+	$('button').each(function(item){
+		$(this).click(function()
+		{
+			var localCount=0;
+			return function(e)
+			{
+				var me=$(this);
+				me.css("color","blue");
+				console.log(item);
+				var value=item;//this line shud b replaced..Time complexity shud b o(n)
+				$("#contents").text(value);
+				$("#counter").text("count value is : "+(++localCount)+" total_count value : "+(++count));
+				$("body").attr("class","hide_bg");
+				if(localCount>10)
+				{
+					$("body").attr("class","show_bg");
+				}
+			}
+		}());
+	});	
 });
+
+	
+
